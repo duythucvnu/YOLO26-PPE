738981d3eba4371c9b571ef3bdfc9cab7ab6b474
Thuc Nguyen Duy <nguyenduythuc1608@gmail.com>
2026-04-14 01:27:16 +0700

    Update tasks.py

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -1558,18 +1558,161 @@ def load_checkpoint(weight, device=None, inplace=True, fuse=False):
     return model, ckpt
 
 
-def parse_model(d, ch, verbose=True):
-    """Parse a YOLO model.yaml dictionary into a PyTorch model.
-
+def parse_model(d, ch, verbose=True):  # model_dict, input_channels(3)
+    """
+    Parse a YOLO model.yaml dictionary into a PyTorch model.
     Args:
         d (dict): Model dictionary.
         ch (int): Input channels.
         verbose (bool): Whether to print model details.
-
     Returns:
-        (torch.nn.Sequential): PyTorch model.
-        (list): Sorted list of layer indices whose outputs need to be saved.
+        (tuple): Tuple containing the PyTorch model and sorted list of output layers.
     """
+    import ast
+ 
+    # Args
+    max_channels = float("inf")
+    nc, act, scales = (d.get(x) for x in ("nc", "activation", "scales"))
+    depth, width, kpt_shape = (d.get(x, 1.0) for x in ("depth_multiple", "width_multiple", "kpt_shape"))
+    if scales:
+        scale = d.get("scale")
+        if not scale:
+            scale = tuple(scales.keys())[0]
+            LOGGER.warning(f"WARNING ⚠️ no model scale passed. Assuming scale='{scale}'.")
+        if len(scales[scale]) == 3:
+            depth, width, max_channels = scales[scale]
+        elif len(scales[scale]) == 4:
+            depth, width, max_channels, threshold = scales[scale]
+ 
+    if act:
+        Conv.default_act = eval(act)  # redefine default activation, i.e. Conv.default_act = nn.SiLU()
+        if verbose:
+            LOGGER.info(f"{colorstr('activation:')} {act}")  # print
+ 
+    if verbose:
+        LOGGER.info(f"\n{'':>3}{'from':>20}{'n':>3}{'params':>10}  {'module':<60}{'arguments':<50}")
+    ch = [ch]
+    layers, save, c2 = [], [], ch[-1]  # layers, savelist, ch out
+    is_backbone = False
+    for i, (f, n, m, args) in enumerate(d["backbone"] + d["head"]):  # from, number, module, args
+        try:
+            if m == 'node_mode':
+                m = d[m]
+                if len(args) > 0:
+                    if args[0] == 'head_channel':
+                        args[0] = int(d[args[0]])
+            t = m
+            m = getattr(torch.nn, m[3:]) if 'nn.' in m else globals()[m]  # get module
+        except:
+            pass
+        for j, a in enumerate(args):
+            if isinstance(a, str):
+                with contextlib.suppress(ValueError):
+                    try:
+                        args[j] = locals()[a] if a in locals() else ast.literal_eval(a)
+                    except:
+                        args[j] = a
+        n = n_ = max(round(n * depth), 1) if n > 1 else n  # depth gain
+        if m in {
+            Classify, Conv, ConvTranspose, GhostConv, Bottleneck, GhostBottleneck, SPP, SPPF, DWConv, Focus,
+            BottleneckCSP, C1, C2, C2f, ELAN1, AConv, SPPELAN, C2fAttn, C3, C3TR,
+            C3Ghost, nn.Conv2d, nn.ConvTranspose2d, DWConvTranspose2d, C3x, RepC3, PSA, SCDown,
+        }:
+            if args[0] == 'head_channel':
+                args[0] = d[args[0]]
+            c1, c2 = ch[f], args[0]
+            if c2 != nc:  # if c2 not equal to number of classes (i.e. for Classify() output)
+                c2 = make_divisible(min(c2, max_channels) * width, 8)
+            if m is C2fAttn:
+                args[1] = make_divisible(min(args[1], max_channels // 2) * width, 8)  # embed channels
+                args[2] = int(
+                    max(round(min(args[2], max_channels // 2 // 32)) * width, 1) if args[2] > 1 else args[2]
+                )  # num heads
+ 
+            args = [c1, c2, *args[1:]]
+ 
+        elif m in {AIFI, FocalModulation}:
+            args = [ch[f], *args]
+            c2 = args[0]
+        elif m in (HGStem, HGBlock):
+            c1, cm, c2 = ch[f], args[0], args[1]
+            if c2 != nc:  # if c2 not equal to number of classes (i.e. for Classify() output)
+                c2 = make_divisible(min(c2, max_channels) * width, 8)
+                cm = make_divisible(min(cm, max_channels) * width, 8)
+            args = [c1, cm, c2, *args[2:]]
+            if m in (HGBlock):
+                args.insert(4, n)  # number of repeats
+                n = 1
+        elif m is ResNetLayer:
+            c2 = args[1] if args[3] else args[1] * 4
+        elif m is nn.BatchNorm2d:
+            args = [ch[f]]
+        elif m is Concat:
+            c2 = sum(ch[x] for x in f)
+        elif m is BiFPN_Concat2:
+            c2 = sum(ch[x] for x in f)
+        elif m is BiFPN_Concat3:
+            c2 = sum(ch[x] for x in f)
+        elif m is CARAFE:
+            c1 = ch[f]
+            c2 = c1
+            args = [c1, *args]
+        elif m in {MLLAttention}:
+            c2 = ch[f]
+            args = [c2, *args]
+        elif m in frozenset({Detect, WorldDetect, Segment, Pose, OBB, ImagePoolingAttn, v10Detect}):
+            args.append([ch[x] for x in f])
+        elif m is RTDETRDecoder:  # special case, channels arg must be passed in index 1
+            args.insert(1, [ch[x] for x in f])
+        elif m is CBLinear:
+            c2 = make_divisible(min(args[0][-1], max_channels) * width, 8)
+            c1 = ch[f]
+            args = [c1, [make_divisible(min(c2_, max_channels) * width, 8) for c2_ in args[0]], *args[1:]]
+        elif m is CBFuse:
+            c2 = ch[f[-1]]
+        elif isinstance(m, str):
+            t = m
+            if len(args) == 2:
+                m = timm.create_model(m, pretrained=args[0], pretrained_cfg_overlay={'file': args[1]},
+                                      features_only=True)
+            elif len(args) == 1:
+                m = timm.create_model(m, pretrained=args[0], features_only=True)
+            c2 = m.feature_info.channels()
+        elif m in {repvit_m0_9, repvit_m1_0, repvit_m1_1, repvit_m1_5, repvit_m2_3,}:
+            m = m(*args)
+            c2 = m.channel
+        else:
+            c2 = ch[f]
+ 
+ 
+        if isinstance(c2, list):
+            is_backbone = True
+            m_ = m
+            m_.backbone = True
+        else:
+            m_ = nn.Sequential(*(m(*args) for _ in range(n))) if n > 1 else m(*args)  # module
+            t = str(m)[8:-2].replace('__main__.', '')  # module type
+        m.np = sum(x.numel() for x in m_.parameters())  # number params
+        m_.i, m_.f, m_.type = i + 4 if is_backbone else i, f, t  # attach index, 'from' index, type
+        if verbose:
+            LOGGER.info(f"{i:>3}{str(f):>20}{n_:>3}{m.np:10.0f}  {t:<60}{str(args):<50}")  # print
+        save.extend(x % (i + 4 if is_backbone else i) for x in ([f] if isinstance(f, int) else f) if
+                    x != -1)  # append to savelist
+        layers.append(m_)
+        if i == 0:
+            ch = []
+        if isinstance(c2, list):
+            ch.extend(c2)
+            for _ in range(5 - len(ch)):
+                ch.insert(0, 0)
+        else:
+            ch.append(c2)
+    return nn.Sequential(*layers), sorted(save)
+
+
+"""
+def parse_model(d, ch, verbose=True):
+
     import ast
 
     # Args
@@ -1736,7 +1879,7 @@ def parse_model(d, ch, verbose=True):
             ch.append(c2)
             
     return torch.nn.Sequential(*layers), sorted(save)
-
+"""
 
 def yaml_model_load(path):
     """Load a YOLO model from a YAML file.
